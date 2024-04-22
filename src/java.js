fba9e23f28833a76623404b189bed95a4a7f6a05
Ayanda295 <mthembuayanda295@gmail.com>
2024-04-22 20:58:30 +0200

    added basic css

---
+++ src/java.js
@@ -0,0 +1,50 @@
+function updateWeather(response) {
+    let temperatureElement = document.querySelector("#temperature")
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = '${response.data.temperature.humidity}%';
+    windSpeedElement.innerHTML = '${response.data.wind.speed}km/h';
+    temperatureElement.innerHTML = Math.round(temperature);
+    timeElement.innerHTML = formatDate (date);
+    timeElement.innerHTML = '${date.getDay()} ${date.getHours()}:${date.getMinutes()}';
+}
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#app-city");
+    cityElement.innerHTML = searchInput.ariaValueMax;
+}
+
+function searchCity(city) {
+ let apiKey = "e9ba0e93do7684t3cff3919ff4b00bf7";
+ let apiUrl = "https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}";
+  console.log (apiUrl);
+ axios.get(apiUrl).then(updateWeather); 
+}
+
+let searchformElement = document.querySelector("#search-form");
+searchformElement.addEventListener("submit", handleSearchSubmit);
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
+let timeElement = document.querySelector("#time");
+let date = newDate (response.data.time *1000);
+
+function formatDate (date) {
+    let day = days[date.getDay()];
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    if (minutes < 10) {
+        minutes = '0${minutes}';
+    }
+    return '${day} ${hours}:${minutes}';
+}
+
+let icon = document.querySelector("icon");
+iconElement.innerHTML = '<img src= "${response.data.condition.icon_url}" class = weather-app-icon" />'
